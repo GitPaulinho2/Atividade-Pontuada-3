2c1bda805843fdc1f5c2a17bff3c7e57cdf29ced
Diego Bras <150878045+GitPaulinho2@users.noreply.github.com>
2025-05-05 18:44:24 +0000

    Salvando

--- 1.0 Atividade_Pontuada.py
+++ 1.0 Atividade_Pontuada.py
@@ -4,6 +4,7 @@ os.system("clear")
 matricula = input("Digite a sua matrícula: ")
 senha = int(input("Digite sua senha: "))
 salario = float(input("Digite o seu sálario: "))
+acrescimo_refeicao = 0
 
 vale_refeicao = int(input("Digite o valor do vale refeição: "))
 def vale_refeicao_recebido():
@@ -17,25 +18,46 @@ match vale_transporte:
         case "n":
                print()     
 dependentes = int(input("Informe a quantidade de dependentes: "))
+def calcular_plano_saude(dependentes):
+    return dependentes * 150.00
 def calcular_inss(salario):
-    if salario <= 1518.08:
-         return salario * 0.075
-    elif salario <= 2793.88:
-         return salario * 0.09
-    elif salario <= 4190.83:
-         return salario * 0.12
-    elif salario <= 8157.41:
-         return salario * 0.14	
-    
-def calcular_irrf(salario):
-      if salario <= 2259.21:
-        return salario * 0.075
-      elif salario <= 2826.65:
-        return salario * 0.09
-      elif salario <= 4190.83:
-        return salario * 0.12
-      elif salario <= 8157.41:
-        return salario * 0.14	
+    match salario:
+        case _ if salario <= 1518.00:
+            return salario * 0.075
+        case _ if salario <= 2793.88:
+            return salario * 0.09 - 113.85
+        case _ if salario <= 4190.83:
+            return salario * 0.12 - 189.54
+        case _ if salario <= 8157.41:
+            return salario * 0.14 - 318.38
+        case _:
+            return 1142.04
+    	
+def calcular_irrf(salario, dependentes):
+    base_calculo = salario - (189.59 * dependentes)
+    match base_calculo:
+        case _ if base_calculo <= 2259.20:
+            return 0  
+        case _ if base_calculo <= 2826.65:
+            return base_calculo * 0.075 - 169.44
+        case _ if base_calculo <= 3751.05:
+            return base_calculo * 0.15 - 381.44
+        case _ if base_calculo <= 4664.68:
+            return base_calculo * 0.22 - 662.77
+        case _:
+            return base_calculo * 0.275 - 896.00
+	
+def calcular_salario_liquido(salario_base, dependentes, vale_transporte, vale_refeicao):
+    calcular_inss = calcular_inss(salario_base)
+    calcular_irrf = calcular_irrf(salario_base, dependentes)
+    vale_transporte = vale_transporte(salario_base, vale_transporte)
+    vale_refeicao_recebido = vale_refeicao_recebido(vale_refeicao)
+    desconto_plano_saude = calcular_plano_saude(dependentes)
+
+    salario = salario_liquido - (calcular_inss + calcular_irrf + vale_transporte + vale_refeicao_recebido + desconto_plano_saude)
+    return salario_liquido
+
+salario_liquido = calcular_salario_liquido(salario, dependentes, vale_transporte, vale_refeicao)
 
 print(f"Matricula: {matricula}")
 print(f"Senha: {senha}")
@@ -43,3 +65,4 @@ print(f"Sálario Líquido: {salario}")
 print(f"Vale Refeição: {vale_refeicao}")
 print(f"Dependentes: {dependentes}")
 print(f"Vale Transporte: {vale_transporte}")
+print(f"Sálario Total: {salario_liquido}")
